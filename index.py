cd17c946b22dfb509d9287200e7b795f4ba770be
Rubén Darío Carrillo López <rubendario199201@gmail.com>
2023-10-14 13:10:42 -0500

    Create Index.py

---
+++ index.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+# Nombre de la Aplicacion
+app = Flask(__name__)
+
+app.route("/")
+def home():
+    return 'Pagina de Inicio'
+
+if __name__ == '__main__':
+    app.run(debug=True)
